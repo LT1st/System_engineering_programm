7374cfa18086333c01cfac7ef71130980876651a
lt1st <1417274896@qq.com>
2022-06-14 00:35:34 +0800

    add visulization of rout

--- code/visualization/from_matrix.py
+++ code/visualization/from_matrix.py
@@ -44,7 +44,57 @@ def from_matrix(distance_matrix):
     nx.draw_networkx(G, pos, node_size=400)
     plt.show()
 
-    return 
+    return G
+
+def root_from_list(root_list, distance_matrix, auto=False):
+    """
+    root_list:
+        最优路径，保存为list，记得得走成闭环
+    distance_matrix:
+        距离矩阵
+    auto:
+        自动规划路径
+    """
+    distance_matrix = np.array(distance_matrix)
+
+    G = from_matrix(distance_matrix)
+    pos = nx.get_node_attributes(G, "pos")
+
+    # Depot should be at (0,0)
+    pos[0] = (0.5, 0.5)
+
+    H = G.copy()
+
+
+    #   计算路径
+    for i in range(len(pos)):
+        for j in range(i + 1, len(pos)):
+            dist = math.hypot(pos[i][0] - pos[j][0], pos[i][1] - pos[j][1])
+            dist = dist
+            G.add_edge(i, j, weight=dist)
+    # 自动规划路径
+    if auto:
+        cycle = nx_app.christofides(G, weight="weight")
+        edge_list = list(nx.utils.pairwise(cycle))
+    else:
+        edge_list = root_list
+
+    # 画全连接网
+    nx.draw_networkx_edges(H, pos, edge_color="blue", width=0.5)
+
+    # 画路径
+    nx.draw_networkx(
+        G,
+        pos,
+        with_labels=True,
+        edgelist=edge_list,
+        edge_color="red",
+        node_size=200,
+        width=3,
+    )
+
+    #print("The route of the traveller is:", cycle)
+    plt.show()   
 
 if __name__ == "__main__":
     distance_matrix = np.array([
@@ -55,5 +105,7 @@ if __name__ == "__main__":
         [51, 78, 68, 51, 0, 13],
         [60, 70, 68, 61, 13, 0]
     ])
+    root = [0,1,3,2,5,4,0]
 
     from_matrix(distance_matrix)
+    root_from_list(root,distance_matrix)
